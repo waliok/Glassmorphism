ad2328b4f4bbc4a8372afd50c9448feae5d53e23
Valentyn <waliok2292@gmail.com>
2023-02-26 00:07:05 +0100

    Initial Commit

---
+++ Glassmorphism_IOS15/Glassmorphism_IOS15App.swift
@@ -0,0 +1,17 @@
+//
+//  Glassmorphism_IOS15App.swift
+//  Glassmorphism_IOS15
+//
+//  Created by Waliok on 26/02/2023.
+//
+
+import SwiftUI
+
+@main
+struct Glassmorphism_IOS15App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
